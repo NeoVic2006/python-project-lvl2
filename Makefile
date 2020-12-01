a08356ba66a6dd8e7f54b94ed27225facf219ce0
NeoVic2006 <NeoVic2006@gmail.com>
2020-12-01 12:18:34 -0600

    step4. Linter, GitAct

---
+++ Makefile
@@ -0,0 +1,6 @@
+install:
+		@poetry install
+
+lint:
+		poetry run flake8 gendiff
+
--- gendiff/scripts/files_check.py
+++ gendiff/scripts/files_check.py
@@ -1,13 +1,12 @@
 import json
 
 
-
-file_path1 = json.load(open('C:\Python_Work_for_Hexlet\python-project-lvl2\\files\\file1.json'))
-file_path2 = json.load(open('C:\Python_Work_for_Hexlet\python-project-lvl2\\files\\file2.json'))
+file_path1 = json.load(open('C:\\Python_Work_for_Hexlet\\python-project-lvl2\\files\\file1.json'))
+file_path2 = json.load(open('C:\\Python_Work_for_Hexlet\\python-project-lvl2\\files\\file2.json'))
 
 
 def generate_diff(file_path1, file_path2):
-    result = ('{ ' + '\n') 
+    result = ('{ ' + '\n')
     unique_keys_file2 = file_path2.keys() - file_path1.keys()
 
     if len(unique_keys_file2) != 0:
@@ -17,17 +16,17 @@ def generate_diff(file_path1, file_path2):
     for key, value in file_path1.items():
         if key in file_path2:
             if value == file_path2[key]:
-                result = result + ('\n' + "    " + str(key) + ": " + str(file_path2[key]))
+                result = result + ('\n' + "    " +
+                                   str(key) + ": " + str(file_path2[key]))
             else:
-                result = result + ('\n' + "  - " + str(key) + ": " + str(value))
-                result = result + ('\n' + "  + " + str(key) + ": " + str(file_path2[key]))
+                result = result + ('\n' + "  - " +
+                                   str(key) + ": " + str(value))
+                result = result + ('\n' + "  + " +
+                                   str(key) + ": " + str(file_path2[key]))
         else:
-            result = result + ('\n' + "  - " + str(key) + ": " + str(value))       
+            result = result + ('\n' + "  - " + str(key) + ": " + str(value))
     result = result + ('\n' + ' }')
+    return result
 
-    return result 
 
 print(generate_diff(file_path1, file_path2))
-
-
-

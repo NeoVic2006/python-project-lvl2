7210f8bc17c6757cd03b0763a4158e812e03226c
NeoVic2006 <NeoVic2006@gmail.com>
2020-12-01 11:43:34 -0600

    Starting step 4

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,5 @@
 import argparse
-import json
+
 
 
 def main():
@@ -8,19 +8,12 @@ def main():
     parser.add_argument("second_file")
     parser.add_argument("-f","--format", help="set format of output")
     args = parser.parse_args()
-    return print(args.echo)
-
-'''
-file_path1 = json.load(open('C:\Python_Work_for_Hexlet\python-project-lvl2\\files\\file1.json'))
-
-
-def generate_diff(file_path1):
-    print(file_path1)
+    return args.echo
 
 
 
-generate_diff(file_path1)
-'''
+
+
 if __name__ == '__main__':
     main()
 

05c5618f80ca30da14639ce3824a57a78b01991a
NeoVic2006 <NeoVic2006@gmail.com>
2020-12-03 09:49:06 -0600

    Github action Tests

--- tests/fixtures/files_check_tests.py
+++ tests/fixtures/files_check_tests.py
@@ -1,21 +1,20 @@
 import json
-from files_check import generate_diff
 
+from gendiff.scripts.files_check import generate_diff
 
 def test_answer():
 
- 
+
     # Ubuntu version
     file_path1 = json.load(open('/mnt/c/Python_Work_for_Hexlet/files/file1.json'))
     file_path2 = json.load(open('/mnt/c/Python_Work_for_Hexlet/files/file2.json'))
-
     '''
+    
     # CMD version 
-    entries = os.listdir('C:\\Python_Work_for_Hexlet\\files\\')
     file_path1 = json.load(open('C:\\Python_Work_for_Hexlet\\files\\file1.json'))
     file_path2 = json.load(open('C:\\Python_Work_for_Hexlet\\files\\file2.json'))
-    print(entries)
     '''
+    
 
     result = '{ \n  + verbose: True\n    host: hexlet.io\n  - timeout: 50\n  + timeout: 20\n  - proxy: 123.234.53.22\n  - follow: False\n}'
     assert generate_diff(file_path1, file_path2) == result

44a41232aebb8297f259dcb2c16eed729ca4bef2
NeoVic2006 <NeoVic2006@gmail.com>
2020-12-03 09:54:06 -0600

    Github action Tests

--- tests/fixtures/files_check_tests.py
+++ tests/fixtures/files_check_tests.py
@@ -1,10 +1,10 @@
 import json
+from files_check import generate_diff
 
-from gendiff.scripts.files_check import generate_diff
 
 def test_answer():
 
-
+    '''
     # Ubuntu version
     file_path1 = json.load(open('/mnt/c/Python_Work_for_Hexlet/files/file1.json'))
     file_path2 = json.load(open('/mnt/c/Python_Work_for_Hexlet/files/file2.json'))
@@ -13,7 +13,7 @@ def test_answer():
     # CMD version 
     file_path1 = json.load(open('C:\\Python_Work_for_Hexlet\\files\\file1.json'))
     file_path2 = json.load(open('C:\\Python_Work_for_Hexlet\\files\\file2.json'))
-    '''
+    
     
 
     result = '{ \n  + verbose: True\n    host: hexlet.io\n  - timeout: 50\n  + timeout: 20\n  - proxy: 123.234.53.22\n  - follow: False\n}'

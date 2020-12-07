9fdcc4ac986c00613ff28914ed5fb8fd36e720bf
Sergey Vengilevskiy <NeoVic2006@gmail.com>
2020-12-07 12:04:46 -0600

    testing test

--- tests/fixtures/files_check_tests.py
+++ tests/fixtures/files_check_tests.py
@@ -3,19 +3,19 @@ from gendiff_file import generate_diff
 
 
 def test_answer():
-    '''
+    
     file_path1 = json.load(open('/home/runner/work/python-project-lvl2/python-project-lvl2/gendiff/files/file1.json'))
     file_path2 = json.load(open('/home/runner/work/python-project-lvl2/python-project-lvl2/gendiff/files/file2.json'))
-
+    '''
     # Ubuntu version
     file_path1 = json.load(open('/mnt/c/Python_Work_for_Hexlet/python-project-lvl2/tests/fixtures/files/file1.json'))
     file_path2 = json.load(open('/mnt/c/Python_Work_for_Hexlet/python-project-lvl2/tests/fixtures/files/file2.json'))
-    '''
+    
     # CMD version 
     file_path1 = json.load(open('C:\\Python_Work_for_Hexlet\\python-project-lvl2\\tests\\fixtures\\files\\file1.json'))
     file_path2 = json.load(open('C:\\Python_Work_for_Hexlet\\python-project-lvl2\\tests\\fixtures\\files\\file2.json'))
-    
-   
+    '''
+    print("testing")
     result = '{ \n  + verbose: True\n    host: hexlet.io\n  - timeout: 50\n  + timeout: 20\n  - proxy: 123.234.53.22\n  - follow: False\n}'
     assert generate_diff(file_path1, file_path2) == result
 

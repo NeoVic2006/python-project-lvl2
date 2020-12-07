79a6d5dd44001d5c03ec981358f890b78862e613
Sergey Vengilevskiy <NeoVic2006@gmail.com>
2020-12-07 12:01:43 -0600

    fixing everything

--- gendiff/gendiff_file.py
+++ gendiff/gendiff_file.py
@@ -2,15 +2,22 @@ import json
 
 
 def generate_diff(file_name1, file_name2):
+
+    json_file_1 = json.load(open('C:\\Python_Work_for_Hexlet\\python-project-lvl2\\tests\\fixtures\\files\\file1.json'))
+    json_file_2 = json.load(open('C:\\Python_Work_for_Hexlet\\python-project-lvl2\\tests\\fixtures\\files\\file2.json'))
+
+    '''
     json_file_1 = read_json_file(file_name1)
     json_file_2 = read_json_file(file_name2)
+    '''
+
     result = ('{ ' + '\n')
 
     unique_keys_file2 = json_file_2.keys() - json_file_1.keys()
 
     if len(unique_keys_file2) != 0:
         for key in unique_keys_file2:
-            result = result + ("  + " + str(key) + ": " + str(json_file_2[key]))
+            result = result + ("  + "+str(key)+": " + str(json_file_2[key]))
     for key, value in json_file_1.items():
         if key in json_file_2:
             if value == json_file_2[key]:
@@ -26,12 +33,11 @@ def generate_diff(file_name1, file_name2):
     result = result + ('\n' + '}')
     return result
 
-
-
+'''
 def read_json_file(filename):
+    print(filename)
     with open(filename) as json_file:
         return json.load(json_file)
 
-
-
-#generate_diff('file1.json', 'file2.json') 
+# generate_diff('file1.json', 'file2.json')
+'''
--- gendiff/scripts/gendiff_launcher.py
+++ gendiff/scripts/gendiff_launcher.py
@@ -3,13 +3,13 @@ from gendiff_file import generate_diff
 
 
 def main():
-    print("MAIN SCRIPT IS STARTED SUCCESFULLY")
     parser = argparse.ArgumentParser(description="New Generated diff")
     parser.add_argument("first_file")
     parser.add_argument("second_file")
     parser.add_argument("-f", "--format", help="set format of output")
     args = parser.parse_args()
     result = generate_diff(args.first_file, args.second_file)
+    print(result)
     return result
 
 

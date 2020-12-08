074b1fece12ac2667549117ccff5734125e3b23c
Sergey Vengilevskiy <NeoVic2006@gmail.com>
2020-12-08 15:34:54 -0600

    Testing step 4

--- tests/fixtures/files_check_tests.py
+++ tests/fixtures/files_check_tests.py
@@ -24,6 +24,7 @@ def test_answer():
     result = '{ \n  + verbose: True\n    host: hexlet.io\n  - timeout: 50\n  + timeout: 20\n  - proxy: 123.234.53.22\n  - follow: False\n}'
     assert generate_diff(file_path1, file_path2) == result
 
+
 if __name__ == '__main__':
     test_answer()
 

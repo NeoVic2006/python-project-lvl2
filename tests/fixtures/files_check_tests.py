aa6efcaa94795ffd1c28ea91587802ce21fea13b
Sergey Vengilevskiy <NeoVic2006@gmail.com>
2020-12-09 11:23:27 -0600

    Small updates to code

--- tests/fixtures/files_check_tests.py
+++ tests/fixtures/files_check_tests.py
@@ -7,18 +7,18 @@ def test_answer():
     path = os.getcwd()
     print(path)
 
-    '''
+    
     file_path1 = json.load(open('/home/runner/work/python-project-lvl2/python-project-lvl2/gendiff/files/file1.json'))
     file_path2 = json.load(open('/home/runner/work/python-project-lvl2/python-project-lvl2/gendiff/files/file2.json'))
-    
+    '''
     # Ubuntu version
     file_path1 = json.load(open('/mnt/c/Python_Work_for_Hexlet/python-project-lvl2/tests/fixtures/files/file1.json'))
     file_path2 = json.load(open('/mnt/c/Python_Work_for_Hexlet/python-project-lvl2/tests/fixtures/files/file2.json'))
-    '''
+    
     # CMD version 
     file_path1 = json.load(open('file1.json'))
     file_path2 = json.load(open('file2.json'))
-    
+    '''
 
 
 

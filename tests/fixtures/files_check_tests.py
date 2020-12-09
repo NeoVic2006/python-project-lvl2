23faa62fdec4d521c2eeb3a1e0b0ebc904355862
Sergey Vengilevskiy <NeoVic2006@gmail.com>
2020-12-09 11:20:27 -0600

    Small updates to code

--- tests/fixtures/files_check_tests.py
+++ tests/fixtures/files_check_tests.py
@@ -20,6 +20,8 @@ def test_answer():
     file_path2 = json.load(open('files/file2.json'))
     
 
+
+
     print("testing")
     result = '{ \n  + verbose: True\n    host: hexlet.io\n  - timeout: 50\n  + timeout: 20\n  - proxy: 123.234.53.22\n  - follow: False\n}'
     assert generate_diff(file_path1, file_path2) == result

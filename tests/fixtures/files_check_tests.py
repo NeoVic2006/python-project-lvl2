b79c378cdc8b272beba9037d90f6d453ef28951a
Sergey Vengilevskiy <NeoVic2006@gmail.com>
2020-12-09 14:16:05 -0600

    Fixes

--- tests/fixtures/files_check_tests.py
+++ tests/fixtures/files_check_tests.py
@@ -7,7 +7,7 @@ def test_answer():
     print(path)
 
     result = '{ \n  + verbose: True\n    host: hexlet.io\n  - timeout: 50\n  + timeout: 20\n  - proxy: 123.234.53.22\n  - follow: False\n}'
-    assert generate_diff("files/file1.json", "files/file2.json") == result
+    assert generate_diff("file1.json", "file2.json") == result
 
 
 if __name__ == '__main__':

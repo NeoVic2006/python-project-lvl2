2021e364aac8f129465a14c2e83c3df5e6d15fcb
Sergey Vengilevskiy <NeoVic2006@gmail.com>
2020-12-09 11:29:03 -0600

    Small updates to code

--- tests/fixtures/files_check_tests.py
+++ tests/fixtures/files_check_tests.py
@@ -8,8 +8,8 @@ def test_answer():
     print(path)
 
     
-    file_path1 = json.load(open('/home/runner/work/python-project-lvl2/python-project-lvl2/python-project-lvl2/tests/fixtures/files/file1.json'))
-    file_path2 = json.load(open('/home/runner/work/python-project-lvl2/python-project-lvl2/python-project-lvl2/tests/fixtures/files/file2.json'))
+    file_path1 = json.load(open('files/file1.json'))
+    file_path2 = json.load(open('/home/runner/work/python-project-lvl2/python-project-lvl2/tests/fixtures/files/file2.json'))
     '''
     # Ubuntu version
     file_path1 = json.load(open('/mnt/c/Python_Work_for_Hexlet/python-project-lvl2/tests/fixtures/files/file1.json'))

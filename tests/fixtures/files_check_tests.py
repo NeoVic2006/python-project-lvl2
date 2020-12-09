b305ed61ab6687ecfbca4752cffabc8623369b3f
Sergey Vengilevskiy <NeoVic2006@gmail.com>
2020-12-09 11:40:43 -0600

    Small updates to code

--- tests/fixtures/files_check_tests.py
+++ tests/fixtures/files_check_tests.py
@@ -10,7 +10,7 @@ def test_answer():
     '''
     file_path1 = json.load(open('/home/runner/work/python-project-lvl2/python-project-lvl2/tests/fixtures/files/file1.json'))
     file_path2 = json.load(open('/home/runner/work/python-project-lvl2/python-project-lvl2/tests/fixtures/files/file2.json'))
-    
+    '''
     # Ubuntu version
     file_path1 = json.load(open('/mnt/c/Python_Work_for_Hexlet/python-project-lvl2/tests/fixtures/files/file1.json'))
     file_path2 = json.load(open('/mnt/c/Python_Work_for_Hexlet/python-project-lvl2/tests/fixtures/files/file2.json'))
@@ -18,7 +18,7 @@ def test_answer():
     # CMD version 
     file_path1 = json.load(open('files/file1.json'))
     file_path2 = json.load(open('files/file2.json'))
-    
+    '''
 
     print("testing")
     result = '{ \n  + verbose: True\n    host: hexlet.io\n  - timeout: 50\n  + timeout: 20\n  - proxy: 123.234.53.22\n  - follow: False\n}'

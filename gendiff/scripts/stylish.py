2be0607a125c643b66819ef24d2a145ec9974ec2
Sergey Vengilevskiy <NeoVic2006@gmail.com>
2020-12-14 16:10:34 -0600

    Stylish added and tested

--- gendiff/scripts/stylish.py
+++ gendiff/scripts/stylish.py
@@ -1,41 +1,32 @@
-
 status = {
     'new': "+",
     'old': "-",
     'same': " "
 }
 
-
-
-def stylish(file):
-
-    string = '{'
+def stylish(file, spaces=2):
+    string = ' {'
     for i in file:
-        string += "\n" + status[i['status']] + " " + str(i["name"])
-        if isinstance(i["value"], list):
-            string += stylish(i["value"])
-        else:
-            string += ": " + str(i["value"])
-    string += "\n}"
+        if isinstance(i["value"], dict):
+            string += "\n" + spaces*" " + status[i['status']] + " " + str(i["name"]) + unpaking_dict(i["value"], spaces + 4)
+        else: 
+            string += "\n" + spaces*" " + status[i['status']] + " " + str(i["name"])
+            if isinstance(i["value"], list):
+                string += stylish(i["value"], spaces + 4)
+            else:
+                string += ": " + str(i["value"])
+
+    string += "\n" + spaces*" " + "}"
     return string
 
 
-
-
-
-
-
-
-
-
-
-def stylish123(file):
-
-    string = '{'
-    for i in file:
-        if isinstance(i["value"], list):
-            string += " \n" + "  " + str(status[i['status']]) + str(i["name"]) + " " + stylish(i["value"])
+def unpaking_dict(dictionary, spaces):
+    string = ' {'
+    for i in dictionary:
+        if isinstance(dictionary[i], dict):
+            string += "\n" + spaces*" "+ " " + str(i)
+            string += unpaking_dict(dictionary[i], spaces + 4)
         else:
-            string += " \n" + "      " + str(status[i['status']]) + " " + str(i["name"]) + ": " + str(i["value"])
-    string += " \n }"
+            string += "\n" + spaces*" " + str(" " + i + ": " + str(dictionary[i]))
+    string += "\n" + spaces*" " + "}"
     return string

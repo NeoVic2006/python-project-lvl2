26a52e9feca501ec51eb09017a13cad091465d86
Sergey Vengilevskiy <NeoVic2006@gmail.com>
2020-12-21 16:36:05 -0600

    Improving quality of code

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -16,7 +16,8 @@ def _get_diff(file1, file2):
             result.append({"name": key,
                            "value": _get_diff(file1[key], file2[key]),
                            "status": "same"})
-        if file1[key] == file2[key]:
+        
+        elif file1[key] == file2[key]:
             result.append(status_tree(file2, key, "same", "same"))
         else:
             result.append(status_tree(file1, key, "same", "changed_old"))

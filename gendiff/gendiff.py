d736dd3bb238648fc8bf9c79c86995b6f30c9b02
Sergey Vengilevskiy <NeoVic2006@gmail.com>
2020-12-21 11:09:13 -0600

    Fixing Main function

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -4,17 +4,18 @@ from gendiff.scripts.check_file_extention import check_extension
 
 def generate_diff(file1, file2):
     file1, file2 = check_extension(file1, file2)
+    return test_func(file1, file2)
 
+
+def test_func(file1, file2):
     new_keys = file2.keys() - file1.keys()
     old_keys = file1.keys() - file2.keys()
     same_keys = file1.keys() - new_keys - old_keys
-
     result = []
-
     for i in same_keys:
         if isinstance(file1[i], dict) and isinstance(file2[i], dict):
             result.append({"name": i,
-                           "value": generate_diff(file1[i], file2[i]),
+                           "value": test_func(file1[i], file2[i]),
                            "status": "same"})
         else:
             if file1[i] == file2[i]:
@@ -34,7 +35,7 @@ def generate_diff(file1, file2):
 def _single_file_check(i, file, status):
     if isinstance(file[i], dict):
         return {"name": i,
-                "value": generate_diff(file[i], file[i]),
+                "value": test_func(file[i], file[i]),
                 "status": status}
     else:
         return {"name": i, "value": file[i], "status": status}
--- tests/tests.py
+++ tests/tests.py
@@ -1,13 +1,12 @@
-from gendiff.gendiff import generate_diff_JSON, generate_diff_YAML
+from gendiff.gendiff import generate_diff
 from gendiff.formatters.formatter import formatters
-from gendiff.scripts.check_file_extention import check_extension
 
 
 def test_formatter_stylish():
     with open("tests/answers/_formatter_stylish.txt", "r") as f:
         answer = f.read()
-    result_JSON_stylish = formatters(generate_diff_JSON("tests/fixtures/json_files/file1.json", "tests/fixtures/json_files/file2.json"), "stylish")
-    result_YAML_stylish = formatters(generate_diff_YAML("tests/fixtures/yaml_files/file1.yml", "tests/fixtures/yaml_files/file2.yml"), "stylish")
+    result_JSON_stylish = formatters(generate_diff("tests/fixtures/json_files/file1.json", "tests/fixtures/json_files/file2.json"), "stylish")
+    result_YAML_stylish = formatters(generate_diff("tests/fixtures/yaml_files/file1.yml", "tests/fixtures/yaml_files/file2.yml"), "stylish")
     assert result_JSON_stylish == answer
     assert result_YAML_stylish == answer
 
@@ -15,8 +14,8 @@ def test_formatter_stylish():
 def test_formatter_plain():
     with open("tests/answers/_formatter_plain.txt", "r") as f:
         answer = f.read()
-    result_JSON_stylish = formatters(generate_diff_JSON("tests/fixtures/json_files/file1recursive.json", "tests/fixtures/json_files/file2recursive.json"), "plain")
-    result_YAML_stylish = formatters(generate_diff_YAML("tests/fixtures/yaml_files/file1recursive.yml", "tests/fixtures/yaml_files/file2recursive.yml"), "plain")
+    result_JSON_stylish = formatters(generate_diff("tests/fixtures/json_files/file1recursive.json", "tests/fixtures/json_files/file2recursive.json"), "plain")
+    result_YAML_stylish = formatters(generate_diff("tests/fixtures/yaml_files/file1recursive.yml", "tests/fixtures/yaml_files/file2recursive.yml"), "plain")
     assert result_JSON_stylish == answer
     assert result_YAML_stylish == answer
 
@@ -24,26 +23,16 @@ def test_formatter_plain():
 def test_formatter_json():
     with open("tests/answers/_formatter_JSON.txt", "r") as f:
         answer = f.read()
-    result_JSON_stylish = formatters(generate_diff_JSON("tests/fixtures/json_files/file1recursive.json", "tests/fixtures/json_files/file2recursive.json"), "json")
-    result_YAML_stylish = formatters(generate_diff_YAML("tests/fixtures/yaml_files/file1recursive.yml", "tests/fixtures/yaml_files/file2recursive.yml"), "json")
+    result_JSON_stylish = formatters(generate_diff("tests/fixtures/json_files/file1recursive.json", "tests/fixtures/json_files/file2recursive.json"), "json")
+    result_YAML_stylish = formatters(generate_diff("tests/fixtures/yaml_files/file1recursive.yml", "tests/fixtures/yaml_files/file2recursive.yml"), "json")
     assert result_JSON_stylish == answer
     assert result_YAML_stylish == answer
-
-
-def test_check_extension():
-    with open("tests/answers/file_extention.txt", "r") as f:
-        answer = f.read()
-    result_json_files = check_extension("tests/fixtures/json_files/file1recursive.json", "tests/fixtures/json_files/file2recursive.json")
-    result_yaml_files = check_extension("tests/fixtures/yaml_files/file1recursive.yml", "tests/fixtures/yaml_files/file2recursive.yml")
-    assert str(result_json_files) == answer
-    assert str(result_yaml_files) == answer
 
 
 if __name__ == '__main__':
     test_formatter_plain()
     test_formatter_stylish()
     test_formatter_json()
-    test_check_extension()
 
 
 # pytest -q tests/tests.py

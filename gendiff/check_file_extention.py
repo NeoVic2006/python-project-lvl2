8dd537459386729f0a4a262160b13a3adcce4019
Sergey Vengilevskiy <NeoVic2006@gmail.com>
2020-12-28 11:11:50 -0600

    Fixing Extentions

--- gendiff/check_file_extention.py
+++ gendiff/check_file_extention.py
@@ -6,7 +6,7 @@ from yaml.scanner import ScannerError
 
 
 def check_extension(file_name):
-    root_ext = os.path.splitext(file_name) 
+    root_ext = os.path.splitext(file_name)
     if root_ext[1] == '.json':
         try:
             with open(file_name, 'r') as file:
@@ -19,6 +19,6 @@ def check_extension(file_name):
             with open(file_name, 'r') as file:
                 return yaml.load(file, Loader=yaml.FullLoader)
         except ScannerError:
-            raise RuntimeError("This is not valid Yaml file")     
+            raise RuntimeError("This is not valid Yaml file")
     else:
         raise RuntimeError("This file extention is incorrect")
